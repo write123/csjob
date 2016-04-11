d479987e2995647def31967f6233ac49d0f64d73
zhangxiuyun <zhangxiuyun@shinezone.com>
2016-04-11 13:18:17 +0800

    aaaaaaa

--- proj.android/obj/local/armeabi/objs-debug/cocos2dx_static/support/ccUtils.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs-debug/cocos2dx_static/support/ccUtils.o: \
- D:\wamp\www\quick-cocos2d-x\lib\cocos2d-x/cocos2dx/support/ccUtils.cpp \
- D:\wamp\www\quick-cocos2d-x\lib\cocos2d-x/cocos2dx/support/ccUtils.h
-
-D:\wamp\www\quick-cocos2d-x\lib\cocos2d-x/cocos2dx/support/ccUtils.h:
