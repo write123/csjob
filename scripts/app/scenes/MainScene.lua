a98bb91312616f5697a8c2a1327485fbbc44963a
zhangxiuyun <zhangxiuyun@shinezone.com>
2016-04-13 16:55:19 +0800

    adddd

--- scripts/app/scenes/MainScene.lua
+++ scripts/app/scenes/MainScene.lua
@@ -1,7 +1,3 @@
-local cur = nil
-local total = nil
-local _data = {}
-
 local MainScene = class("MainScene", function()
     return display.newScene("MainScene")
 end)
@@ -12,205 +8,132 @@ MainScene.SLIDER_IMAGES = {
 }
 
 function MainScene:ctor()
+    self.cur_topic_number = 1
+    self.final_data = {}
+    self.cur_topic_data = {}
 
-	self.cur = 0
-	self.total = 20
+ 	display.newSprite("background.png"):pos(display.cx, display.cy):addTo(self)
 
- 	-- display.newColorLayer(ccc4(255,255,255,255)):addTo(self)
- 	display.newSprite("background.png")
- 		:pos(display.cx, display.cy)
- 		:addTo(self)
+    self.cur_topic_data = self:getQuestionData()
 
  	self:initTopLabel()
-    self:refushQuestion()
+    self:initTopic()
+    self:showButtomItem()
+end
 
- 	cc.ui.UIPushButton.new("Button01.png", {scale9 = true})
- 		:setButtonSize(160, 60)
- 		:setButtonLabel(cc.ui.UILabel.new({text = "返回菜单"}))
+function MainScene:showButtomItem()
+    cc.ui.UIPushButton.new("Button01.png", {scale9 = true})
+        :setButtonSize(160, 60)
+        :setButtonLabel(cc.ui.UILabel.new({text = "返回菜单"}))
         :pos(display.left + 120, display.bottom + 80)
- 		:onButtonClicked(function()
- 			app:enterScene("MenuScene", nil, "pageTurn", 0.5)
- 		end)
- 		:onButtonPressed(function(event)
+        :onButtonClicked(function()
+            app:enterScene("MenuScene", nil, "pageTurn", 0.5)
+        end)
+        :onButtonPressed(function(event)
             event.target:setScale(1.1)
         end)
         :onButtonRelease(function(event)
             event.target:setScale(1.0)
         end)
- 		:addTo(self)
+        :addTo(self)
 
- 	cc.ui.UIPushButton.new("Button01.png", {scale9 = true})
- 		:setButtonSize(160, 60)
- 		:setButtonLabel(cc.ui.UILabel.new({text = "下一题"}))
+    cc.ui.UIPushButton.new("Button01.png", {scale9 = true})
+        :setButtonSize(160, 60)
+        :setButtonLabel(cc.ui.UILabel.new({text = "下一题"}))
         :pos(display.right - 120, display.bottom + 80)
- 		:onButtonClicked(function()
-            self:refushQuestion()
- 		end)
- 		:onButtonPressed(function(event)
+        :onButtonClicked(function()
+            self.cur_topic_number = self.cur_topic_number + 1
+            self.cur_topic_data = self:getQuestionData()
+            self:reflush()
+        end)
+        :onButtonPressed(function(event)
             event.target:setScale(1.1)
         end)
         :onButtonRelease(function(event)
             event.target:setScale(1.0)
         end)
- 		:addTo(self)
+        :addTo(self)
 end
 
-function MainScene:init()
-    if self:getChildByTag(51) then
-        self:removeChildByTag(51)
-    end
+function MainScene:initTopic()
+    -- title
+    ui.newTTFLabel({ text = self.cur_topic_data.title, size = 24})
+        :pos(display.cx, display.cy+200)
+        :addTo(self)
+        :setTag(51)
 
-    if self:getChildByTag(52) then
-        self:removeChildByTag(52)
-    end
+    -- describe
+    ui.newTTFLabel({text = "(" .. self.cur_topic_data.describe .. ")", size = 18, color = ccc3(255,0,0), })
+        :pos(display.cx, display.cy + 200 - 30)
+        :addTo(self)
+        :setTag(52)
 
-    if self:getChildByTag(61) then
-        self:removeChildByTag(61)
-    end
+    local valueLabel = ui.newTTFLabel({text = "", size = 14, color = display.COLOR_BLACK, align = ui.TEXT_ALIGN_CENTER})
+        :pos(display.cx, display.cy + 200 - 100)
+        :addTo(self)
+    
+    valueLabel:setTag(61)
 
-    if self:getChildByTag(62) then
-        self:removeChildByTag(62)
-    end
-    -- body
+    self.final_data[self.cur_topic_number] = {t = self.cur_topic_data.type, s = 0}
+    local uislider = cc.ui.UISlider.new(display.LEFT_TO_RIGHT, MainScene.SLIDER_IMAGES, {scale9 = true, max = 5, min = 1})
+        :onSliderValueChanged(function(event)
+                local score =  string.format("%d", event.value)
+                valueLabel:setString(score .. "分")
+                self.final_data[self.cur_topic_number].s = score
+            end)
+        :setSliderSize(400, 30)
+        :setSliderValue(2)
+        :align(display.CENTER, display.cx, display.cy + 200 - 160 + 30 / 2)
+        :addTo(self)
+
+    uislider:setTag(62)
 end
-
-function MainScene:refushQuestion()
-    self.cur = self.cur + 1
-    local data = self:getQuestionData()
-    local curLabel = self:getChildByTag(41)
-    curLabel:setString(self.cur)
-    _data[self.cur] = { type = data.type, score = 0}
-
-    self:reflush(data)
-
-    dump(_data)
-end
-
 
 function MainScene:getQuestionData()
-    require("framework.functions")
-    require("framework.json")
 
     local filePath = "configs/cs_question.json"
     local data = {}
 
-    if io.exists(filePath) then 
-        local questionFile = io.readfile(filePath)
+    local questionFile = CCFileUtils:sharedFileUtils():getFileData(filePath)
+    if questionFile then 
         local questionJson = json.decode(questionFile)
-
-        data = questionJson[self.cur]
+        data = questionJson[self.cur_topic_number]
     end
 
     return data
 end
 
-function MainScene:reflush(data)
-        self:init()
-        
-        -- title
-        ui.newTTFLabel({
-            text = data.title, 
-            size = 24, 
-            align = ui.TEXT_ALIGN_LEFT
-        })
-        :pos(display.cx, display.cy+200)
-        :addTo(self)
-        :setTag(51)
-
-        -- describe
-        ui.newTTFLabel({
-            text = "(" .. data.describe .. ")", 
-            size = 18, 
-            align = ui.TEXT_ALIGN_LEFT,
-            color = ccc3(255,0,0), 
-        })
-        :pos(display.cx, display.cy + 200 - 30)
-        :addTo(self)
-        :setTag(52)
-
-        local barHeight = 30
-        local barWidth = 400
-        local valueLabel = ui.newTTFLabel({text = "", size = 14, color = display.COLOR_BLACK, align = ui.TEXT_ALIGN_CENTER})
-            :pos(display.cx, display.cy + 200 - 100)
-            :addTo(self)
-        
-        valueLabel:setTag(61)
-
-        local score = 0
-        local uislider = cc.ui.UISlider.new(display.LEFT_TO_RIGHT, MainScene.SLIDER_IMAGES, {scale9 = true, max = 5, min = 1})
-            :onSliderValueChanged(function(event)
-                    score =  string.format("%d", event.value)
-                    valueLabel:setString(score)
-                    _data[self.cur].score = score
-                end)
-            :setSliderSize(barWidth, barHeight)
-            :setSliderValue(2)
-            :align(display.CENTER, display.cx, display.cy + 200 - 160 + barHeight / 2)
-            :addTo(self)
-
-        uislider:setTag(62)
+function MainScene:reflush()
+        self:getChildByTag(41):setString(self.cur_topic_number)
+        self:getChildByTag(51):setString(self.cur_topic_data.title)
+        self:getChildByTag(52):setString(self.cur_topic_data.describe)
+        self:getChildByTag(61):setString("")
+        self:getChildByTag(62):setSliderValue(2)
 end
-
-local function callback(event)  
-    local ok = (event.name == "completed")  
-    local request = event.request  
-    local response = request:getResponseString()  
-     print(response)  
-    local json=require("framework.shared.json")  
-    local t=json.decode(response)  
-    print(t)  
-end
-
 
 function MainScene:initTopLabel()
-    ui.newTTFLabel({
-    	text = "当前第", 
-    	size = 24, 
-    	align = ui.TEXT_ALIGN_CENTER
-    	})
+    ui.newTTFLabel({text = "当前第", size = 24,})
         :pos(display.left + 120, display.top - 40)
         :addTo(self)
 
-    local curLabel = ui.newTTFLabel({
-    	text = self.cur,
-    	color = ccc3(255,0,0), 
-    	size = 22, 
-    	align = ui.TEXT_ALIGN_CENTER
-    	})
+    ui.newTTFLabel({text = self.cur_topic_number,color = ccc3(255,0,0), size = 22, align = ui.TEXT_ALIGN_CENTER})
         :pos(display.left + 170, display.top - 42)
         :addTo(self)
+        :setTag(41)
 
-    curLabel:setTag(41)
-
-    ui.newTTFLabel({
-    	text = "题", 
-    	size = 24, 
-    	align = ui.TEXT_ALIGN_CENTER
-    	})
+    ui.newTTFLabel({text = "题", size = 24, })
         :pos(display.left + 200, display.top - 40)
         :addTo(self)
 
-    ui.newTTFLabel({
-    	text = "共", 
-    	size = 24, 
-    	align = ui.TEXT_ALIGN_CENTER
-    	})
+    ui.newTTFLabel({text = "共", size = 24, })
         :pos(display.right - 180, display.top - 40)
         :addTo(self)
 
-    ui.newTTFLabel({
-    		text = self.total, 
-    		color = ccc3(255,0,0), 
-    		size = 24, 
-    		align = ui.TEXT_ALIGN_CENTER
-    }):pos(display.right - 150, display.top - 42)
+    ui.newTTFLabel({text = "20",color = ccc3(255,0,0), size = 24, align = ui.TEXT_ALIGN_CENTER})
+      :pos(display.right - 150, display.top - 42)
       :addTo(self)
 
-    ui.newTTFLabel({
-    	text = "题", 
-    	size = 24, 
-    	align = ui.TEXT_ALIGN_CENTER
-    	})
+    ui.newTTFLabel({text = "题", size = 24, })
         :pos(display.right - 120, display.top - 40)
         :addTo(self)
 end
